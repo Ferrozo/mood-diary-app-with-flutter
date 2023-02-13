b8bd3a56861ad69771119a75bd0d48201d708a6b
Ferrozo <ferrozo.c@gmail.com>
2023-02-13 16:46:42 +0100

    add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mood_diary_app_with_flutter/src/views/pages/exports_pages.dart';
 
 void main() {
   testWidgets('Initialization App', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const MaterialApp());
   });
 }
